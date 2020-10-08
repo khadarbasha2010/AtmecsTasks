4efc80094718f254aa6f81df032e08439f298d9a
Khadar Basha <khadarbasha2010@gmail.com>
2020-10-09 01:42:15 +0530

    Source Code Added

---
+++ ATMECS Task One/ATMECS Task One/Actions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ATMECS_Task_One
+{
+    
+    internal static class Actions
+    {
+        public static Semaphore _semaphore = new Semaphore(1,1);
+        // Action 1
+        public static void Action1()
+        {
+            _semaphore.WaitOne();
+            Console.WriteLine($"Action 1 Started");            
+            // For Checking
+            Thread.Sleep(3000);
+            Console.WriteLine($"Action 1 Completed");
+            _semaphore.Release();
+        }
+        // Action 2
+        public static void Action2()
+        {
+            _semaphore.WaitOne();
+            Console.WriteLine($"Action 2 Started");
+            
+            // For Checking
+            Thread.Sleep(3000);
+            Console.WriteLine($"Action 2 Completed");
+            _semaphore.Release();
+        }
+        // Action 3
+        public static void Action3()
+        {
+            _semaphore.WaitOne();
+            Console.WriteLine($"Action 3 Started");
+            
+            // For Checking
+            Thread.Sleep(3000);
+            Console.WriteLine($"Action 3 Completed");
+            _semaphore.Release();
+        }
+        // Action 4
+        public static void Action4()
+        {
+            _semaphore.WaitOne();
+            Console.WriteLine($"Action 4 Started");
+            
+            // For Checking
+            Thread.Sleep(3000);
+            Console.WriteLine($"Action 4 Completed");
+            _semaphore.Release();
+        }
+    }
+}
---
+++ ATMECS Task One/ATMECS Task One/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ATMECS_Task_One
+{
+    public delegate void threaddelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Thread> myThreads = new List<Thread>();
+            myThreads.Add(new Thread(Actions.Action1));
+            myThreads.Add(new Thread(Actions.Action2));
+            myThreads.Add(new Thread(Actions.Action3));
+            myThreads.Add(new Thread(Actions.Action4));
+            foreach (var CurrentThread in myThreads)
+            {               
+                CurrentThread.Start();
+            }           
+            Console.ReadKey();
+         }
+
+        
+
+    }
+}
